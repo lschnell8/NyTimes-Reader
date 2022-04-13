6f36dd016e61dd0eaf16d33e2575b69edfaaf7d9
lschnell8 <lschnell8@gmail.com>
2022-04-12 18:11:00 -0600

    Import useEffect and implement fetch call to obtain data from API

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useEffect } from 'react';
+import { fetchData } from '../../apiCalls';
+import { Route, Routes } from 'react-router-dom';
+import Header from '../Header/Header';
+import ArticleDetails from '../ArticleDetails/ArticleDetails';
+import Articles from '../Articles/Articles';
 
-function App() {
+const App = () => {
+
+  useEffect (() => {
+    fetchData('world')
+      .then(response => {
+        if (response.ok) {
+          console.log(response)
+          return response.json();
+        } else {
+          console.log(response.status)
+          return response.status
+      }
+      })
+      .then(data => {
+      console.log(data.results)
+      })
+      .catch(error => {
+      console.log(error)
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Routes>
+        <Route path='/' element={ <Header /> } />
+        <Route path='/' element={ <Articles /> } />
+        <Route path='details' element={ <ArticleDetails /> } />
+    </Routes>
+  )
+};
 
 export default App;
