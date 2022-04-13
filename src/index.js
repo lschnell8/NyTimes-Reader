30a5c7140cd89a4eb3be06793811d5871752501b
lschnell8 <lschnell8@gmail.com>
2022-04-12 18:31:10 -0600

    Change root to use v18 syntax and update file type from css to scss

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client'; 
+import './index.scss';
+import App from '../src/Components/App/App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+const rootElement = document.getElementById('root');
+ReactDOMClient.createRoot(rootElement).render(<BrowserRouter>
+  <App />
+</BrowserRouter>);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
