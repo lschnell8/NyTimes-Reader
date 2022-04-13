5cb69a403b5254e87a268585b1f845d5626dd344
lschnell8 <lschnell8@gmail.com>
2022-04-12 18:34:23 -0600

    Add component and build function to display article details

---
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -0,0 +1,15 @@
+import './ArticleDetails.scss';
+
+const ArticleDetails = () => {
+  return (
+    <section>
+      <h2>Title</h2>
+      <p>Details</p>
+      <p>Details</p>
+      <p>Details</p>
+      <p>Details</p>
+    </section>
+  )
+};
+
+export default ArticleDetails;
