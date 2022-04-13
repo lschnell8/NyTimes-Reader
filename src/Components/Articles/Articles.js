9b3cec6215554d897380a97d2f327b36f1aeee59
lschnell8 <lschnell8@gmail.com>
2022-04-12 18:32:50 -0600

    Build container to display article summaries

--- src/Components/Articles/Articles.js
+++ src/Components/Articles/Articles.js
@@ -1,1 +1,18 @@
-import ''
+import './Articles.scss';
+import '../ArticleSummary/ArticleSummary';
+import ArticleSummary from '../ArticleSummary/ArticleSummary';
+
+const Articles = (stories) => {
+  const news = stories.map(story => {
+    return (
+      <ArticleSummary key={ story.id } title={ story.title } details={ story.details } />
+    )
+  })
+  return (
+    <section>
+      { news }
+    </section>
+  )
+};
+
+export default Articles;
