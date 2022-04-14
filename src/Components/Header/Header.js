12c1cac481192eee3f9e8e3f290eae6a45994418
lschnell8 <lschnell8@gmail.com>
2022-04-14 01:57:50 -0600

    Remove console logs and commented out code

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,11 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    // <Routes>
       <Link to='/'>
         <h1>NY TIMES READER</h1>
       </Link>
-    // </Routes>
   )
 };
 
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,10 @@
 export const fetchData = (section) => {
-  const options = {
-    method: "GET",
-    headers: {
-      "Accept": "application/json"
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${process.env.REACT_APP_NYT_KEY}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        return response.status
     }
-  };
-  return fetch(`https://api.nytimes.com/svc/topstories/v2/${ section }.json?api-key=ZBS8HsBHmKgKHq7zG91kZQnQksJwxNmg`, options)
+    })
 };
