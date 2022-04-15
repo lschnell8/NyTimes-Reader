2079bebad9ce49f6cf2fa7b34c2e76dbeb9b68ae
lschnell8 <lschnell8@gmail.com>
2022-04-15 06:51:22 -0600

    Add wrapper elements and classnames for styling

--- src/Components/ArticleSummary/ArticleSummary.js
+++ src/Components/ArticleSummary/ArticleSummary.js
@@ -1,18 +1,23 @@
 import './ArticleSummary.scss';
 import { Link } from 'react-router-dom';
 
-const ArticleSummary = ({ url, title, abstract, created_date, multimedia }) => {
+const ArticleSummary = ({ url, title, abstract, created_date, multimedia, section }) => {
+  console.log('summary location', )
   if (url === 'null' || !title) {
     console.log('SKIP')
   } else {
     return (
-      <article className='article-card'>
-        <Link to={`${created_date}`}>
+      <Link
+        to={`${created_date}`}
+        className='article-card'>
+        <div className='title-details'>
           <h3>{title}</h3>
           <p>{abstract}</p>
+        </div>
+        <div className='thumbnail'>
           {multimedia?<img src={multimedia[2].url} alt={multimedia[2].caption} />:<p>No Image Available</p>}
-        </Link>
-      </article>
+        </div>
+      </Link>
     )
   }
 };
