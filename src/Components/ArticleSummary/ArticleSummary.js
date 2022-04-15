4d7ef6d55068d1eb0c2269231010c95b2899a62a
lschnell8 <lschnell8@gmail.com>
2022-04-14 22:54:43 -0600

    Add data content and conditionally render based on existence

--- src/Components/ArticleSummary/ArticleSummary.js
+++ src/Components/ArticleSummary/ArticleSummary.js
@@ -1,13 +1,18 @@
 import './ArticleSummary.scss';
 import { Link } from 'react-router-dom';
 
-const ArticleSummary = ({ title, abstract, subsection }) => {
-  return (
-      <Link to={ subsection }>
-      <h2>{ title }</h2>
-      <p>{ abstract }</p>
+const ArticleSummary = ({ url, title, abstract, created_date, multimedia }) => {
+  if (url === 'null' || !title) {
+    console.log('SKIP')
+  } else {
+    return (
+      <Link to={`${created_date}`}>
+        <h2>{title}</h2>
+        <p>{abstract}</p>
+        {multimedia?<img src={multimedia[2].url} alt={multimedia[2].caption} />:<p>No Image Available</p>}
       </Link>
-  )
+    )
+  }
 };
 
 export default ArticleSummary;
