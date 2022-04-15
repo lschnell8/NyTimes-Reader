ef34c82fab4ea5356210e99e442fcae63975a7f3
lschnell8 <lschnell8@gmail.com>
2022-04-14 23:08:07 -0600

    Restructure, move useEffect back to App, topics to new component, and add outlet to display article summaries within home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,40 +1,12 @@
 import './Home.scss';
-import { useState, useEffect } from 'react';
-import { fetchData } from '../../apiCalls';
-import Articles from '../Articles/Articles';
-import { Link } from 'react-router-dom';
-
+import Topics from '../Topics/Topics';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
-  const topics = ['arts', 'automobiles', 'books', 'business', 'fashion', 'food', 'health', 'home', 'insider', 'magazine', 'movies', 'nyregion', 'obituaries', 'opinion', 'politics', 'realestate', 'science', 'sports', 'sundayreview', 'technology', 'theater', 't-magazine', 'travel', 'upshot', 'us', 'world'];
-  const topicsDisplay = topics.map((topic, i) => {
-    return (
-      <Link to={topic}>
-        <p key={ i }>{ topic }</p>
-      </Link>
-    )
-  })
-
-  const [newsStories, setNewsStories] = useState([]);
-  const [topic, setTopic] = useState('home')
-  // const [err, setError] = useState(0);
-
-  useEffect (() => {
-    fetchData(topic)
-      .then(data => {
-        setNewsStories([...data.results])
-        return newsStories
-      })
-      
-    console.log('useEffect', newsStories)
-  }, [])
-
   return (
-    <section>
-      <nav>
-        { topicsDisplay }
-      </nav>
-      <Articles newsStories={newsStories} />
+    <section className='body'>
+      <Topics />
+      <Outlet />
     </section>
   )
 };
