21469216dad9479458350660c2e151b82926e2aa
lschnell8 <lschnell8@gmail.com>
2022-04-14 22:51:06 -0600

    Add content from data to display and link full NY Times article

--- src/Components/ArticleDetails/ArticleDetails.js
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -1,13 +1,23 @@
 import './ArticleDetails.scss';
+// import { Link } from 'react-router-dom';
 
-const ArticleDetails = () => {
+const ArticleDetails = ({ article }) => {
+  console.log('DETAILS', article)
   return (
     <section>
-      <h2>Title</h2>
-      <p>Details</p>
-      <p>Details</p>
-      <p>Details</p>
-      <p>Details</p>
+      <h2>{article.title}</h2>
+          <section>
+            <img src={`${article.multimedia[1].url}`} alt={`${article.multimedia[1].caption}`} />
+            <p>{article.multimedia[1].caption}</p>
+            <p>{article.multimedia[1].copyright}</p>
+          </section>
+      <p>{article.byline}</p>
+      <p>{article.published_date}</p>
+      {/* <Link reloadDocument replace={article.url}>
+        <p>{article.url}</p>
+      </ Link> */}
+       <a target='_blank' rel='noreferrer' href={`${article.url}`}>{`${article.url}`}</a>
+      {/* <Link to={`${article.url}`} target='_blank' rel='noreferrer' >{`${article.url}`}</Link> */}
     </section>
   )
 };
