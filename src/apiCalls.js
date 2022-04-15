4dbe06aaacae3fe0ea60f611637453b5013da0a6
lschnell8 <lschnell8@gmail.com>
2022-04-15 06:30:15 -0600

    Move response to App

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,3 @@
 export const fetchData = (section) => {
   return fetch(`https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${process.env.REACT_APP_NYT_KEY}`)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return response.status
-      }
-    })
 };
